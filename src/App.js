701e765f6e02a299800216ad494450d2ee1328a2
Luciene Santos <lucienesantosmc@gmail.com>
2018-10-23 07:24:43 -0300

    Move procider to app return

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
-import Board from './components/board';
+import React, {Component} from "react";
+import "./App.css";
+import Board from "./components/Board";
+
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {Reducers} from "./reducers";
+
+export const store = createStore(Reducers);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-        <Board />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header" />
+          <Board />
+        </div>
+      </Provider>
     );
   }
 }
